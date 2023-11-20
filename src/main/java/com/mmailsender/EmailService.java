e6fd85920f39553e2b1e8fd9221c1b3ef5bab3fa
Shoaib Shabir <AIwithshoaib@gmail.com>
2023-11-20 11:15:59 +0530

    Update EmailService.java

--- src/main/java/com/mmailsender/EmailService.java
+++ src/main/java/com/mmailsender/EmailService.java
@@ -1,38 +1,38 @@
-package com.mmailsender;
+// package com.mmailsender;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.mail.javamail.JavaMailSender;
+// import org.springframework.mail.javamail.MimeMessageHelper;
+// import org.springframework.stereotype.Service;
+// import org.thymeleaf.TemplateEngine;
+// import org.thymeleaf.context.Context;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+// import javax.mail.MessagingException;
+// import javax.mail.internet.MimeMessage;
 
-@Service
-public class EmailService {
+// @Service
+// public class EmailService {
 
-    @Autowired
-    private JavaMailSender emailSender;
+//     @Autowired
+//     private JavaMailSender emailSender;
 
-    @Autowired
-    private TemplateEngine templateEngine;
+//     @Autowired
+//     private TemplateEngine templateEngine;
 
-    public void sendTemplateEmail(String to, String subject, String templateName, Context context) {
-        MimeMessage message = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
+//     public void sendTemplateEmail(String to, String subject, String templateName, Context context) {
+//         MimeMessage message = emailSender.createMimeMessage();
+//         MimeMessageHelper helper = new MimeMessageHelper(message);
 
-        try {
-            helper.setTo(to);
-            helper.setSubject(subject);
+//         try {
+//             helper.setTo(to);
+//             helper.setSubject(subject);
            
-            String content = templateEngine.process(templateName, context);
-            helper.setText(content, true);
+//             String content = templateEngine.process(templateName, context);
+//             helper.setText(content, true);
 
-            emailSender.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace(); 
-        }
-    }
-}
+//             emailSender.send(message);
+//         } catch (MessagingException e) {
+//             e.printStackTrace(); 
+//         }
+//     }
+// }
