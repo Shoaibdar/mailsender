d43d0e1b4b1b49f6e3a5d6dc9425684f743d640e
Shoaib Shabir <AIwithshoaib@gmail.com>
2023-11-23 16:57:56 +0530

    Update EmailController.java

--- src/main/java/com/mmailsender/EmailController.java
+++ src/main/java/com/mmailsender/EmailController.java
@@ -94,7 +94,7 @@ public class EmailController {
 	            //messageMail.setText("body");
 		     try {
 			//String htmlContent = new String(Files.readAllBytes(Paths.get("/templates/email-template.html")));
-			messageMail.setContent("<h1>This is an HTML email</h1><p>It contains some <b>formatted</b> text.</p>", text/html");
+			messageMail.setContent("<h1>This is an HTML email</h1><p>It contains some <b>formatted</b> text.</p>", "text/html");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
